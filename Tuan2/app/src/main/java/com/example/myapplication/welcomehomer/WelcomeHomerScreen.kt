513da8665e3a3428200b3361b67da64fbcbce852
Hieu Nguyen <hehecoi@outlook.com>
2023-09-05 14:56:34 +0700

    Final Week2 Assignment

--- Tuan2/app/src/main/java/com/example/myapplication/welcomehomer/WelcomeHomerScreen.kt
+++ Tuan2/app/src/main/java/com/example/myapplication/welcomehomer/WelcomeHomerScreen.kt
@@ -1,9 +1,17 @@
 package com.example.myapplication.welcomehomer
 
+import android.view.ContextThemeWrapper
 import android.widget.NumberPicker
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
@@ -33,6 +41,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.myapplication.R
 
@@ -53,7 +62,8 @@ fun WelcomeHomerScreen() {
             actions = {
                 Icon(
                     imageVector = Icons.Default.MoreVert,
-                    contentDescription = "More"
+                    contentDescription = "More",
+                    modifier = Modifier.padding(16.dp).clickable {  }
                 )
             }
         )
@@ -78,47 +88,50 @@ private fun WelcomeHomerContainer(modifier: Modifier = Modifier, contentPadding:
     val radioOptions = listOf("PayPal", "Direct")
     val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
     var amount by remember { mutableStateOf("0") }
-    LazyColumn(modifier = modifier, contentPadding = contentPadding) {
+    LazyColumn(modifier = modifier, contentPadding = contentPadding, verticalArrangement = Arrangement.spacedBy(16.dp)) {
         item { Text(text = stringResource(id = R.string.app_title)) }
         item { Text(text = stringResource(R.string.pay_method)) }
         item {
-            Column {
-                radioOptions.forEach { text ->
-                    Row(modifier = Modifier.selectable(selected = (text == selectedOption),
-                        onClick = { onOptionSelected(text) })) {
-                        RadioButton(
-                            selected = (text == selectedOption), onClick = null
-                        )
-                        Text(text = text)
+            Row(modifier = Modifier, horizontalArrangement = Arrangement.SpaceBetween) {
+                Column(modifier = Modifier.weight(0.5f)) {
+                    radioOptions.forEach { text ->
+                        Row(modifier = Modifier.selectable(selected = (text == selectedOption),
+                            onClick = { onOptionSelected(text) })) {
+                            RadioButton(
+                                selected = (text == selectedOption), onClick = null
+                            )
+                            Text(text = text)
+                        }
                     }
                 }
+                val style = if (isSystemInDarkTheme()) R.style.Theme_MyApplication_Dark else R.style.Theme_MyApplication
+                AndroidView(factory = { ctx ->
+                    NumberPicker(ContextThemeWrapper(ctx, style)).apply {
+                        minValue = 0
+                        maxValue = 1000
+                        wrapSelectorWheel = true
+                        setOnValueChangedListener { numberPicker, oldVal, newVal ->
+
+                        }
+                    }
+                }, modifier = Modifier.weight(0.5f))
             }
         }
+        item { Spacer(Modifier.padding(16.dp)) }
+        item { LinearProgressIndicator(progress = 0.25f, modifier = Modifier.fillMaxWidth()) }
         item {
-            AndroidView(factory = { ctx ->
-                NumberPicker(ctx).apply {
-                    minValue = 0
-                    maxValue = 1000
-                    wrapSelectorWheel = true
-                    setOnValueChangedListener { numberPicker, oldVal, newVal ->
-
-                    }
-                }
-            })
-        }
-        item { LinearProgressIndicator(progress = 0.25f) }
-        item {
-            Row {
+            Row(modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
                 Text(text = stringResource(R.string.amount))
                 TextField(value = amount,
                     onValueChange = { text -> amount = text },
                     label = {},
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    singleLine = true
                 )
             }
         }
         item {
-            Row {
+            Row(modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
                 Button(onClick = {}) {
                     Text(text = stringResource(R.string.donate_btn))
                 }
